a6e798aff01f100449a8fd2e7f339adfca8b5756
Cesar Astorga <cesar.antonio.astorga@gmail.com>
2022-04-23 16:41:45 -0300

    Editing urls

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,5 +4,7 @@ from Blog import views
 
 urlpatterns = [
     path('', views.blog, name="Pages"),
-    path('publicar/', views.posteo, name='Posteo')
+    path('publicar/', views.posteo, name='Posteo'),
+    path('pages/', views.leerPosteos, name = "LeerPosteos"),
+    path('post/<title>',views.post, name="Publicacion"),
 ]
